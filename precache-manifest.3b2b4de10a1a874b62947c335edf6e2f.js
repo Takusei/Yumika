fee735150ee1cb6183dd422138c613ee9775d86c
Cai, Zesheng | Jaycen | CTMO <zesheng.a.cai@rakuten.com>
2021-07-09 11:13:51 +0900

    Updates

---
+++ precache-manifest.3b2b4de10a1a874b62947c335edf6e2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a257f271f941f252f26bf6e2804fa833",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a88f0ddaa705a9c3bf51",
+    "url": "./static/css/2.84b3ebfc.chunk.css"
+  },
+  {
+    "revision": "212e05ddaededfa15ce6",
+    "url": "./static/css/main.a50b3f2a.chunk.css"
+  },
+  {
+    "revision": "a88f0ddaa705a9c3bf51",
+    "url": "./static/js/2.b153863e.chunk.js"
+  },
+  {
+    "revision": "0f2be52aac348a57b5e1722f475cbfd0",
+    "url": "./static/js/2.b153863e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "212e05ddaededfa15ce6",
+    "url": "./static/js/main.f0fd1790.chunk.js"
+  },
+  {
+    "revision": "53ab2e534a30e71c6319",
+    "url": "./static/js/runtime-main.36b009c0.js"
+  }
+]);
